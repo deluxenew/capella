2426a0fb007b7009dd9304eeed3f084585354bc4
deluxenew <deluxenew@ya.ru>
2025-12-07 21:34:58 +0300

    fix nuxt image

---
+++ app.config.ts
@@ -0,0 +1,6 @@
+export default defineAppConfig({
+  app: {
+    baseURL: '/capella', // замените на имя вашего репозитория
+    buildAssetsDir: '/_nuxt', // опционально, если нужно изменить путь к ассетам
+  },
+})
