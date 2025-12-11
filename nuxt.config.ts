d37064ecd22b50e1573db28fcedabf8d388379d9
deluxenew <deluxenew@ya.ru>
2025-12-11 08:21:42 +0300

    fix base url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -172,7 +172,7 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       baseURL: process.env.API_DEV_URL || 'http://localhost:3000',
-      apiBase: process.env.API_BASE_URL || 'https://stage-frontend.liquidnow.me/' || 'https://deluxenew.github.io/capella'
+      apiBase: process.env.API_BASE_URL || 'https://stage-frontend.liquidnow.me/'
     }
   },
 })
